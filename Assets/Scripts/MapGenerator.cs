3dcaf901108029b2f7498762507f940a8f2a03d5
Jim Sun <x739566858@outlook.com>
2018-05-12 00:16:26 +0800

    fix: problems of generating map

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
-using UnityEngine.Tilemaps;
 public class MapGenerator : MonoBehaviour {
     public int width;
     public int height;
     public GameObject wall;
     public GameObject floor;
-// Use this for initialization
-void Start () {
-      
+    Collider2D col;
+    // Use this for initialization
+    void Start () {
+        float scale = floor.transform.localScale.x *
+            floor.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        scale -= 0.02f;
         System.Diagnostics.Process p = new System.Diagnostics.Process();
-        p.StartInfo.FileName = "E:\\Demo\\C0D3\\Assets\\Scripts\\test.exe";
+        p.StartInfo.FileName = "D:\\hackpku\\Assets\\Scripts\\test.exe";
         p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
         p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
         p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
@@ -24,9 +26,9 @@ void Start () {
         string retinfo = sr.ReadToEnd();//获得字符串
         Debug.Log(retinfo);
         int[,] tiles = new int[30, 20];
-        TileData data = new TileData();
-
+        col = wall.GetComponent<Collider2D>();
         Debug.Log(retinfo);
+        Debug.Log(scale);
         for (int i = 0; i < 20; i++)
         {
             for (int j = 0; j <= 30; j++)
@@ -38,12 +40,12 @@ void Start () {
                     retinfo[i * 20 + j] == '+')
                 {
                     tiles[j, i] = 0;
-                    Instantiate(floor, new Vector3(j, i, 0), new Quaternion());
+                    Instantiate(floor, new Vector3(j - 15, i - 10, 0) * scale , new Quaternion());
                 }
                 else
                 {
                     tiles[j, i] = 1;
-                    Instantiate(wall, new Vector3(j, i, 0), new Quaternion());
+                    Instantiate(wall, new Vector3(j - 15, i - 10, 0) * scale , new Quaternion());
 
                 }
 
