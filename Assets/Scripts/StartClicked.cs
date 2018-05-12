4d27b7e58c1a7887e184c75a5dea1de379d4e396
xyx <qpalwo@qq.com>
2018-05-12 11:02:41 +0800

    the munu ui

---
+++ Assets/Scripts/StartClicked.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StartClicked : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Button button = this.GetComponent<Button>();
+        EventTrigger eventTrigger = button.gameObject.GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        //   点击事件
+        entry.eventID = EventTriggerType.PointerClick;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnClick(BaseEventData pointData)
+    {
+
+    }
+
+    private void OnMouseEnter(BaseEventData pointData) {
+
+    }
+
+    private void OnMouseExit(BaseEventData pointData) {
+
+    }
+}
